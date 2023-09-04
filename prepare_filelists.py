d698c1a9ec504e7a7fef5ae388c5455e98d3b573
Yaser Abdelaziz <yaser.omar.abdelaziz@gmail.com>
2023-09-04 17:13:47 +0300

    Modify train and inference code

---
+++ prepare_filelists.py
@@ -0,0 +1,34 @@
+# filelists
+#   |-- train.txt
+#   |-- test.txt
+
+import os
+import random
+
+filelist_name = "lombardgrid"
+path = "datasets/lombardgrid_landmarks"
+filelists = ["train.txt", "test.txt"]
+
+test_percent = 0.026
+
+# List folders in the directory
+folders = os.listdir(path)
+for d in folders:
+    folders_in_d = os.listdir(os.path.join(path, d))
+
+    # Set is_test to True test_percent% of the time (randomly)
+    is_test = False
+    if random.random() < test_percent:
+        is_test = True
+
+    for f in folders_in_d:
+        # Create filelists folder if it doesn't exist
+        if not os.path.exists(f"filelists/{filelist_name}"):
+            os.makedirs(f"filelists/{filelist_name}")
+        # If is_test is True, add to test.txt, else add to train.txt
+        if is_test:
+            with open(os.path.join(f"filelists/{filelist_name}", filelists[1]), "a") as test:
+                test.write(os.path.join(d, f) + "\n")
+        else:
+            with open(os.path.join(f"filelists/{filelist_name}", filelists[0]), "a") as train:
+                train.write(os.path.join(d, f) + "\n")
